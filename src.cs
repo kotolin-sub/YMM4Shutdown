5de23500a96d4f50882d9511a38375d81737f2b6
kotolin-sub <tonuma777@gmail.com>
2024-06-01 21:53:57 +0900

    Add files via upload

---
+++ src.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using YukkuriMovieMaker.Plugin;
+using YukkuriMovieMaker.Plugin.Voice;
+
+namespace YMM4Danger
+{
+    public class YMM4Danger : IPlugin
+    {
+        public string Name => "ShutDown";
+
+
+        static void command()
+        {
+            var com = new ProcessStartInfo("shutdown", "shutdown /s /t 0");
+            Process.Start(com);
+        }
+
+    }
+}
